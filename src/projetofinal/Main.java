ce3236147de6ca1605a5a84c45d91089c9f64bfe
ligiasan-tos <ligiaramos0410@gmail.com>
2025-11-11 19:01:40 -0300

    Base do codigo

--- src/projetofinal/Main.java
+++ src/projetofinal/Main.java
@@ -1,13 +1,39 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package projetofinal;
+import java.util.ArrayList;
+import java.util.Scanner;
 
-/**
- *
- * @author aluno
- */
-public class Main {
-    
-}
+public class ProgramaOficina {
+    public static void main(String[] args) {
+        ArrayList<String> servicos = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
+        int opcao;
+
+switch (opcao) {
+                case 1:
+                    cadastrarServico(servicos, scanner);
+                    break;
+                case 2:
+                    listarServicos(servicos);
+                    break;
+                case 3:
+                    imprimirServicos(servicos);
+                    break;
+                case 4:
+                    System.out.println("Saindo...");
+                    break;
+                default:
+                    System.out.println("Opção inválida! Tente novamente.");
+            }
+
+
+        do {
+            System.out.println("MENU OFICINA");
+            System.out.println("1 - Cadastrar serviço");
+            System.out.println("2 - Listar serviços");
+            System.out.println("3 - Imprimir serviços (Tela e TXT)");
+            System.out.println("4 - Sair");
+            System.out.print("Escolha uma opção: ");
+            opcao = scanner.nextInt();
+            scanner.nextLine(); // Limpar o buffer
+
+     } while (opcao != 4);
+
