d44bee2d8e3be9c36533962ff8e9179890c5e83c
Fernando J. <kolumbia3000@gmail.com>
2025-11-12 09:18:22 -0300

    Cadastro de peca

--- src/projetofinal/Main.java
+++ src/projetofinal/Main.java
@@ -1,39 +1,51 @@
-import java.util.ArrayList;
+package projetofinal;
+
 import java.util.Scanner;
 
-public class ProgramaOficina {
+public class Main {
+    public static Scanner scanner = new Scanner(System.in);
+    public static String[][] pecas = new String[100][2];
+    private static int contPecas = 0; 
+
+    public static void cadastrarPeca(){
+        String[] peca = new String[2];
+        peca[0] = String.valueOf(contPecas + 1);
+        System.out.println("----------- Cadastrar Peca -----------------");
+        System.out.println("Digite o nome da peça:");
+        scanner.nextLine();
+        peca[1] = scanner.nextLine();
+        pecas[contPecas][0] = peca[0];
+        pecas[contPecas][1] = peca[1]; 
+        contPecas++;
+    }
+
     public static void main(String[] args) {
-        ArrayList<String> servicos = new ArrayList<>();
-        Scanner scanner = new Scanner(System.in);
-        int opcao;
-
-switch (opcao) {
-                case 1:
-                    cadastrarServico(servicos, scanner);
-                    break;
-                case 2:
-                    listarServicos(servicos);
-                    break;
-                case 3:
-                    imprimirServicos(servicos);
-                    break;
-                case 4:
-                    System.out.println("Saindo...");
-                    break;
-                default:
-                    System.out.println("Opção inválida! Tente novamente.");
-            }
-
-
+        int opcao = 0;
         do {
-            System.out.println("MENU OFICINA");
-            System.out.println("1 - Cadastrar serviço");
-            System.out.println("2 - Listar serviços");
+            
+            System.out.println("------------ MENU OFICINA --------------");
+            System.out.println("1 - Cadastrar peca");
+            System.out.println("2 - Cadastrat servicos");
             System.out.println("3 - Imprimir serviços (Tela e TXT)");
             System.out.println("4 - Sair");
             System.out.print("Escolha uma opção: ");
             opcao = scanner.nextInt();
-            scanner.nextLine(); // Limpar o buffer
-
+            switch (opcao) {
+                    case 1:
+                        cadastrarPeca();
+                        break;
+                    case 2:
+                        //cadastrarServico();
+                        break;
+                    case 3:
+                        //imprimirServicos();
+                        break;
+                    case 4:
+                        System.out.println("Saindo...");
+                        break;
+                    default:
+                        System.out.println("Opção inválida! Tente novamente.");
+                }
      } while (opcao != 4);
-
+    }
+}
